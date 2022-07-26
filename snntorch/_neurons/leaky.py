12d10a22c2eb7caa141dbed8d797e7d94a943a71
lucas-d87u <100155083+lucasDUM@users.noreply.github.com>
2022-07-26 17:45:25 +0100

    Update leaky.py

--- snntorch/_neurons/leaky.py
+++ snntorch/_neurons/leaky.py
@@ -187,8 +187,6 @@ class Leaky(LIF):
             self.reset = self.mem_reset(self.mem)
             self.mem = self.state_fn(input_)
 
-            print("reset")
-            print(self.reset.size())
             if self.state_quant:
                 self.mem = self.state_quant(self.mem)
 
@@ -198,12 +196,8 @@ class Leaky(LIF):
                 self.spk = self.fire(self.mem)
 
             if self.output:  # read-out layer returns output+states
-                print("output")
-                print(self.spk.size())
                 return self.spk, self.mem
             else:  # hidden layer e.g., in nn.Sequential, only returns output
-                print("hidden")
-                print(self.spk.size())
                 return self.spk
 
     def _base_state_function(self, input_, mem):
