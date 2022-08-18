8ecd167d7a4828bb1a82e62ec4229cd0cfdcc1af
lucas-d87u <100155083+lucasDUM@users.noreply.github.com>
2022-08-18 16:05:48 +0100

    Update architectures.py

--- snntorch/architectures.py
+++ snntorch/architectures.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import snntorch as snn
+from snntorch import connections
+
 class MNIST_SNN(nn.Module):
     def __init__(self):
         super().__init__()
