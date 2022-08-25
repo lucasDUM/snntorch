0b8700c1cfb0dadfe78af4a871b8beafb763711a
lucas-d87u <100155083+lucasDUM@users.noreply.github.com>
2022-08-26 00:24:12 +0100

    Update architectures.py

--- snntorch/architectures.py
+++ snntorch/architectures.py
@@ -52,10 +52,13 @@ class MIST_CNN_SNN(nn.Module):
         
         self.linear_burst_1 = nn.Linear(64*4*4, 10)
 
-    def forward(self, x, num_steps):
+        self.num_steps = num_steps
+        self.batch_size = batch_size
+
+    def forward(self, x):
         # Record the final layer
         spk_rec = []
-        for step in range(num_steps):
+        for step in range(self.num_steps):
             start = x[:, step]
 
             current1, threshold = self.conv_burst_1(start)
@@ -64,7 +67,7 @@ class MIST_CNN_SNN(nn.Module):
             current2, threshold = self.conv_burst_2(spk1)
             current2 = F.max_pool2d(current2, 2)
             spk2 = self.lif2(current2)
-            current3, threshold = self.linear_burst_1(spk2.view(batch_size, -1))
+            current3, threshold = self.linear_burst_1(spk2.view(self.batch_size, -1))
             spk3 = self.lif3(current3)
             spk_rec.append(spk3)
 
